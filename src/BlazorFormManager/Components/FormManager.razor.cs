86c7891b7c38a8b4f7876bc2834b9d6cee2c6685
bigabdoul <kababdoul@hotmail.com>
2020-09-05 14:11:10 +0000

    add method NotifyStateChanged()

--- src/BlazorFormManager/Components/FormManager.razor.cs
+++ src/BlazorFormManager/Components/FormManager.razor.cs
@@ -506,6 +506,12 @@ namespace BlazorFormManager.Components
 
         #endregion
 
+        /// <summary>
+        /// Notifies the component that its state has changed.
+        /// When applicable, this will cause the component to be re-rendered.
+        /// </summary>
+        public virtual void NotifyStateChanged() => StateHasChanged();
+
         #region log level
 
         /// <summary>
